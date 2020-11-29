a64778fde16e546cee52b0f13cabe5d87a691072
Agnes Chueng <agneschueng@gmail.com>
2020-11-29 00:26:37 -0600

    compiled wenyan to javacript

---
+++ testing-wenyan(Compiled).js
@@ -0,0 +1,20 @@
+/*Declare an object*/
+var 靜夜思 = {}
+/*Initiate the population of the object*/
+靜夜思 = {
+  /*Define key value pairs*/
+  作者: "李白",
+  種類: "五言絕句",
+  詩句: "床前明月光，疑似地上霜，舉頭望明月，低頭思故鄉。"
+  /*Specify where the object closes*/
+}
+/*Declare a variable (data type needs to be specified)*/
+
+/*string*/
+var 甲 = "胡言亂語"
+/*number*/
+var 乙 = 5
+/*boolean = true*/
+var 丁 = true
+/*boolean = false*/
+var 丙 = false
